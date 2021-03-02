13e9269963ce531126d259a379b2e861746e2296
Anson6118 <et6118.ans@gmail.com>
2021-03-02 22:45:42 +0800

    move to my account

---
+++ Assets/Scripts/ControlArrow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlArrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector2 Velocity = new Vector2(0.0f, 0.0f);
+    public GameObject Shooter;
+    public GameObject Monster;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 CurrentPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 NewPosition = CurrentPosition + Velocity * Time.deltaTime;
+
+        Debug.DrawLine(CurrentPosition, NewPosition, Color.red);
+        
+        RaycastHit2D[] Hits = Physics2D.LinecastAll(CurrentPosition, NewPosition);
+
+        foreach(RaycastHit2D hit in Hits)
+        {
+            GameObject other = hit.collider.gameObject;
+            if(other != Shooter)
+            {
+                if (other.CompareTag("Player"))
+                {
+                    Destroy(gameObject);
+                    Debug.Log(other);
+                    break;
+                }
+
+                if (other.CompareTag("Wall"))
+                {
+                    Destroy(gameObject);
+                    break;
+                }
+
+                if (other.CompareTag("Monster"))
+                {
+                    Destroy(gameObject);
+                    Destroy(other);
+                    break;
+                }
+                
+            }
+        }
+        transform.position = NewPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject arrowPrefab;
+    public GameObject FrontSight;
+    private const float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal")*speed, Input.GetAxis("Vertical")*speed, 0.0f);
+
+        
+
+        /*animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);*/
+
+        transform.position = transform.position + movement * Time.deltaTime;
+        
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shooting();
+        }
+
+    }
+
+    private void Shooting()
+    {
+        float ArrowSpeed = 15.0f;
+        Vector2 PlayerFacing = FrontSight.transform.position - transform.position;
+        PlayerFacing.Normalize();
+        GameObject arrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+        ControlArrow ArrowScript = arrow.GetComponent<ControlArrow>();
+        ArrowScript.Velocity = PlayerFacing * ArrowSpeed;
+        ArrowScript.Shooter = gameObject;
+        arrow.transform.Rotate(0.0f, 0.0f, Mathf.Atan2(PlayerFacing.y, PlayerFacing.x) * Mathf.Rad2Deg);
+        Destroy(arrow, 2.5f);
+    }
+}
